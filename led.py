f6cd51b17fab8dedae18cdaa91b2214fed853e09
juzailml98 <juzailofficial@gmail.com>
2021-09-05 20:48:48 +0530

    added led functionality

--- led.py
+++ led.py
@@ -5,18 +5,20 @@ GPIO.setwarnings(False)
 GPIO.setup(17,GPIO.OUT)
 GPIO.setup(18,GPIO.OUT)
 GPIO.setup(27,GPIO.OUT)
-print ("LED on")
-GPIO.output(17,GPIO.HIGH)
-time.sleep(2)
-print ("LED off")
-GPIO.output(17,GPIO.LOW)
-print ("LED on")
-GPIO.output(18,GPIO.HIGH)
-time.sleep(2)
-print ("LED off")
-GPIO.output(18,GPIO.LOW)
-print ("LED on")
-GPIO.output(27,GPIO.HIGH)
-time.sleep(2)
-print ("LED off")
-GPIO.output(27,GPIO.LOW)
+
+
+def blue():
+    GPIO.output(17,GPIO.HIGH)
+    time.sleep(2)
+
+    GPIO.output(17,GPIO.LOW)
+def green():
+    GPIO.output(18,GPIO.HIGH)
+    time.sleep(2)
+
+    GPIO.output(18,GPIO.LOW)
+def red():
+    GPIO.output(27,GPIO.HIGH)
+    time.sleep(2)
+
+    GPIO.output(27,GPIO.LOW)
