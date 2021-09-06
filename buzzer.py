945b03448498507b99f1593b28bd846a1231caa1
juzailml98 <juzailofficial@gmail.com>
2021-09-06 19:41:45 +0530

    add

--- buzzer.py
+++ buzzer.py
@@ -1,12 +1,13 @@
 import sys
 import RPi.GPIO as GPIO
 import time
+GPIO.setup(triggerPIN,GPIO.OUT)
+buzzer = GPIO.PWM(triggerPIN, 1000)
 def sound(
 
     triggerPIN = 23
-    #GPIO.setmode(GPIO.BCM)
-    GPIO.setup(triggerPIN,GPIO.OUT)
-    buzzer = GPIO.PWM(triggerPIN, 1000) # Set frequency to 1 Khz
+#GPIO.setmode(GPIO.BCM)
+# Set frequency to 1 Khz
     buzzer.start(10) # Set dutycycle to 10
     time.sleep(2)
 #GPIO.cleanup()
