f0f9fdd66c702b600199d3a78bca227439a15555
juzailml98 <juzailofficial@gmail.com>
2021-09-07 16:31:12 +0530

    camera component

--- component-tester.py
+++ component-tester.py
@@ -4,3 +4,16 @@ led.red()
 led.green()
 import buzzer
 buzzer.sound()
+import cv2
+cap = cv2.VideoCapture(0)
+if cap.isOpened():
+    print("camera is working")
+
+
+else:
+    print("camera is not working")
+
+    
+
+cap.release()
+cv2.destroyAllWindows()
